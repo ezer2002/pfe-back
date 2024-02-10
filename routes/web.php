46571876a3c7ebd0a6befcd243a03d5c47f4292a
rania <lakoud_rania7@outlook.fr>
2024-02-10 20:45:38 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AiController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', [AiController::class,'index']);
+
+Route::view('/', 'profile_generators.create');
+
+//Route::post('/generate-profile', [AiController::class,'index']);
+
